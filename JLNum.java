62e981191b40dd71f931d29b6d5d4016bd2b6846
Aman <itsaman1626@gmail.com>
2025-03-25 00:01:41 +0530

    Number array list

---
+++ JLNum.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class JLNum {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+
+        Collections.sort(myNumbers);
+
+        for(int i: myNumbers) {
+            System.out.println(i);
+        }
+    }
+}
